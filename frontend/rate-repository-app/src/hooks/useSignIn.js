5d537cc8c348445630b53be01daa08f4cd190997
Arttu Puttonen <arskaputtonen@gmail.com>
2024-08-08 21:25:50 +0300

    ennen 10.15

--- frontend/rate-repository-app/src/hooks/useSignIn.js
+++ frontend/rate-repository-app/src/hooks/useSignIn.js
@@ -1,10 +1,14 @@
 import { useMutation } from '@apollo/client';
 import { AUTHENTICATE } from '../graphql/mutations';
 import AuthStorage from '../utils/authStorage';
+import { useContext } from 'react';
+import AuthStorageContext from '../contexts/AuthStorageContext';
+import useAuthStorage from './useAuthStorage';
+
 
 const useSignIn = () => {
+  const authStorage = useAuthStorage();
   const [mutate, result] = useMutation(AUTHENTICATE);
-  const authStorage = new AuthStorage();
 
   const signIn = async ({ username, password }) => {
     const { data } = await mutate({ variables: { username, password } });
